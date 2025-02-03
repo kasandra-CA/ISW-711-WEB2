e47d7bf68e7e661dc43942c97f95c2212c59295e
kasandra-CA <115677642+kasandra-CA@users.noreply.github.com>
2025-02-02 22:49:17 -0600

    Modify the app.get in the index.js

--- Workshop-2/server/index.js
+++ Workshop-2/server/index.js
@@ -17,13 +17,23 @@ app.get('/hello', function (req, res) {
 
 
 app.get('/tipocambio', function (req, res) {
-  res.send(`{
-    "TipoCompraDolares" : "608",
-    "TipoVentaDolares" : "621",
-    "TipoCompraEuros" : "731.85",
-    "TipoVentaEuros" : "761.9"
-  }`);
+  const type = req.query.type;
+  
+  if (type === 'usd') {
+    res.send({
+      "TipoCompraDolares" : "608",
+      "TipoVentaDolares" : "621"
+      });
+  } else if (type === 'eur'){
+    res.send({
+      "TipoCompraEuros" : "731.85",
+      "TipoVentaEuros" : "761.9"
+    });
+  } else {
+    res.send({message: "Tipo de moneda no valida"})
+  }
+
 });
 
 
-app.listen(3000, () => console.log(`Example app listening on port 3001!`))
+app.listen(3000, () => console.log(`Example app listening on port 3000!`))
